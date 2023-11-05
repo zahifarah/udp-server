ee91c76a3bf619cf200f6f6468486e76e9acca8c
Zahi Farah <zahi.farah@doctolib.com>
2023-11-05 16:17:06 +0100

    Split and rename stuff

---
+++ helpers.rb
@@ -0,0 +1,36 @@
+def setup_server(ip_address, port)
+  $global_server_ip = ip_address
+  $global_server_port = port
+  server = UDPSocket.new
+  server.bind($global_server_ip, $global_server_port)
+  puts "Server set, cannot speak until spoken to..."
+  return server
+end
+
+def display_sender_details(sender_info)
+  sender_ip = sender_info[2]
+  dynamic_port = sender_info[1]
+  family_address = sender_info[0]
+
+  sender_details = <<~DETAILS
+
+    SENDER INFORMATION:
+    -------------------
+    IP address: #{sender_ip}
+    Dynamic port: #{dynamic_port}
+    Family address: #{family_address}
+
+  DETAILS
+
+  puts sender_details
+end
+
+def process_sender_info(sender_info)
+  sender_ip = sender_info[2]
+
+  # Print sender information and reset the client IP if it's a new sender
+  if $global_client_ip != sender_ip
+    $global_client_ip = sender_ip
+    display_sender_details(sender_info)
+  end
+end
--- server.rb
+++ server.rb
@@ -1,39 +1,11 @@
 require 'socket'
+require_relative "helpers"
 
-$connection = 0
-$client_ip = nil
+$global_server_ip = nil
+$global_server_port = nil
+$global_client_ip = nil
 
-def print_sender_info(sender_addrinfo)
-  sender_ip = sender_addrinfo[2]
-  dynamic_port = sender_addrinfo[1]
-  family_address = sender_addrinfo[0]
-
-  puts
-  puts "SENDER INFORMATION:"
-  puts "-------------------"
-  puts "IP address: #{sender_ip}"
-  puts "Dynamic port: #{dynamic_port}"
-  puts "Family address: #{family_address}"
-  puts
-end
-
-def sender_info(sender_addrinfo)
-  sender_ip = sender_addrinfo[2]
-
-  # Print sender information and reset the client IP if it's a new sender
-  if $client_ip != sender_ip
-    $client_ip = sender_ip
-    print_sender_info(sender_addrinfo)
-  end
-
-  $connection += 1
-end
-
-SERVER_IP = "192.168.1.6"
-SERVER_PORT = 5500
-
-server = UDPSocket.new
-server.bind(SERVER_IP, SERVER_PORT)
+server = setup_server("192.168.1.35", 5500)
 
 client_ip = nil
 client_port = nil
@@ -43,15 +15,14 @@ threads = []
 # This thread will handle receiving messages
 threads << Thread.new do
   loop do
-    text, sender_addrinfo = server.recvfrom(1024)
-
-    sender_info(sender_addrinfo)
+    text, sender_info = server.recvfrom(1024)
+    process_sender_info(sender_info)
 
     puts "#{Time.now.strftime("%T")}: #{text}"
     puts
 
-    client_ip = sender_addrinfo[3]
-    client_port = sender_addrinfo[1]
+    client_ip = sender_info[3]
+    client_port = sender_info[1]
   end
 end
 
@@ -59,7 +30,9 @@ end
 threads << Thread.new do
   loop do
     message_to_send = gets.chomp
-    server.send(message_to_send + "\n", 0, client_ip, client_port) if client_ip && client_port && !message_to_send.empty?
+    if client_ip && client_port && !message_to_send.empty?
+      server.send(message_to_send + "\n", 0, client_ip, client_port)
+    end
   end
 end
 
